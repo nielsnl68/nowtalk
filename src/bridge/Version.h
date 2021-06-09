c67af40c893ac4093009c82abd53582017d0e290
Niels v/d Spek <nielsnl68@gmail.com>
2021-06-09 19:43:53 +0200

    implentage of version control

--- src/bridge/Version.h
+++ src/bridge/Version.h
@@ -1,8 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "2.26.221"
+        #define VERSION "2.26.222"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2021-06-09 17:26:50.873625"
+        #define BUILD_TIMESTAMP "2021-06-09 18:38:46.076452"
     #endif
+    
